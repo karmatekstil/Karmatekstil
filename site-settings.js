9842fb98deee3ec5cbacc94c20d70d58854932e1
karmatekstil <karmatekstil16@gmail.com>
2026-09-23 02:11:51 +0300

    Add files via upload

---
+++ site-settings.js
@@ -0,0 +1,12 @@
+// KARMA TEXTILE - SITE BILGILERI
+// Daha sonra bu dosyadaki tırnak içindeki alanları değiştirerek bağlantıları güncelleyebilirsin.
+const SITE = {
+  phone: "05XX XXX XX XX",
+  whatsapp: "905XXXXXXXXX",
+  address: "Mağaza adresini buraya yaz",
+  maps: "https://maps.google.com/",
+  instagram: "https://instagram.com/",
+  facebook: "https://facebook.com/",
+  trendyol: "#",
+  hours: "09:00 - 19:00"
+};
