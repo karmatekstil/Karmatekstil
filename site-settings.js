74295ee2286187bd5251cf7f98f56642c6afee4e
karmatekstil <karmatekstil16@gmail.com>
2026-09-24 14:52:23 +0300

    Update site-settings.js

--- site-settings.js
+++ site-settings.js
@@ -2,7 +2,7 @@
 // Daha sonra bu dosyadaki tırnak içindeki alanları değiştirerek bağlantıları güncelleyebilirsin.
 const SITE = {
   phone: "05444957349",
-  whatsapp:+905444957349"
+  whatsapp: +905444957349",
   address: "Mağaza adresini buraya yaz",
   maps: "https://maps.google.com/",
   instagram: https://www.instagram.com/karmatekstil?stkn=MTUwcmZoMm00YmF4NQ==
